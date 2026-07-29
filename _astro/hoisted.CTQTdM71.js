b62cf51e55249f03501f1e2cc7006b41809995b1
Frompatagonia <losmarplatenses@hotmail.com>
2026-07-28 21:30:40 -0300

    Fix webpSrc: add base URL prefix for client-side rendering

---
+++ _astro/hoisted.CTQTdM71.js
@@ -0,0 +1,1 @@
+import{c as o}from"./video-audio-bridge.DhESocXV.js";import"./hoisted.CyQfrMbx.js";import"./preload-helper.BB_ImrUI.js";document.addEventListener("DOMContentLoaded",()=>o(document));
